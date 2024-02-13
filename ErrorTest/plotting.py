b5cdf61e11b931d51d9a3c4a652f967f017ec87b
hallma <matthew.hall@wallawalla.edu>
2024-02-13 02:48:35 -0800

    temp mess

--- ErrorTest/plotting.py
+++ ErrorTest/plotting.py
@@ -3,12 +3,38 @@ import parser as p
 import matplotlib.pyplot as plt
 
 
+def pdf(x, points):
+    mean = np.mean(x)
+    std = np.std(x)
+    y_out = 1/(std * np.sqrt(2 * np.pi)) * np.exp( - (points - mean)**2 / (2 * std**2))
+    return y_out
+
+
 def plot(data):
     variance = np.var(data)
     standard = np.std(data)
-    plt.plot(difference, label='Differences')
-    plt.title(f'Variance:{variance}')
-    plt.text(2, -5, 'This is the bottom', ha='center')
+    mean = np.mean(data)
+    points = np.arange(np.min(data), np.max(data), standard/2)  # ok with /2 as long as enough data points, otherwise /4
+    y = pdf(data, np.array(points))
+    plt.figure(figsize=(6, 6))
+    plt.plot(np.array(points), y, color='blue',
+             linestyle='dashed')
+
+    plt.scatter(points, y, marker='o', s=25, color='green', label='STD Curve with STD/2')
+
+    g = pdf(data, data)
+    plt.plot(data, g, color='black',
+             linestyle='solid')
+
+    plt.scatter(data, g, marker='o', s=25, color='red', label='Data Points STD')
+    plt.xlabel('Voltage')
+    plt.ylabel('Probability')
+    var = "{:.2f}".format(variance)
+    std = "{:.2f}".format(standard)
+    men = "{:.2f}".format(mean)
+    plt.title(f'Variance: {var}mV   STD: {std}mV   Mean: {men}mV')
+    plt.legend()
+    plt.grid(True)
     plt.show()
     return
 
